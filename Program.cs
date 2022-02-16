018a5c0a5194bd2a6f57a81645c2d04b48cf33f4
Viney <vineykhaneja999@gmail.com>
2022-02-16 14:21:17 +0530

    [Viney] FEATURE ADDED: Fibonacci Series

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ namespace Logical
         static void Main(string[] args)
         {
             Console.WriteLine("Welocome to Logical Programs");
+            Console.Write("Enter last number of the series: ");
+            //VARIABLES
+            int lastNumber = Convert.ToInt32(Console.ReadLine());
+            int j = 1;
+            int i = 0;
+            int k = 0;
 
+            Console.Write(i + " " + j + " ");
+            while (k < lastNumber - i)
+            {
+                k = j + i;
+                Console.Write(k + " ");
+                i = j;
+                j = k;
+            }            
         }
     }
 }
